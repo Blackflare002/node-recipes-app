5f9734244e09747d749c6d5bdbda3c7720a7d78a
Shane Phillips <shane@loongphillips.com>
2022-06-09 15:04:15 -0400

    nodemon, morgan, first get requests

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let express = require("express");
+let morgan = require("morgan");
 let app = express();
+
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+	res
+		.status(200)
+		.json({ status: 200, message: "This is the server response" });
+});
+
+// this is our catch all endpoint.
+app.get("*", (req, res) => {
+	res.status(404).json({
+		status: 404,
+		message: "This is obviously not what you are looking for.",
+	});
+});
+
 app.listen(3000, () => {
 	console.log("Server running on port 3000");
 });
