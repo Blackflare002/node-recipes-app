a0fb52fe0eb41b7f3619f3e9b10fc2d97830d3e8
Shane Phillips <shane@loongphillips.com>
2022-06-10 16:02:33 -0400

    2nd get request partially functional

--- index.js
+++ index.js
@@ -24,6 +24,37 @@ app.get("/recipes", (req, res) => {
 	});
 });
 
+app.get("/recipes/details/:name", (req, res) => {
+	let inputName = req.params.name;
+	// console.log(typeof inputName);
+	let dataRecipesName = data.recipes.map((el) => {
+		// console.log(el.name);
+		return el.name;
+	});
+	let dataRecipes = data.recipes.map((el) => {
+		let data = el.ingredients + " // ";
+		data += el.instructions + " // ";
+		return data;
+	});
+	// console.log(dataRecipes);
+	let result = null;
+	if (dataRecipesName.includes(inputName)) {
+		result = dataRecipes;
+		res.status(200).json({
+			status: 200,
+			message: "This is the server response",
+			data: result,
+		});
+	} else {
+		result = "{}";
+		res.status(200).json({
+			status: 200,
+			message: "This is the server response",
+			data: result,
+		});
+	}
+});
+
 // this is our catch all endpoint.
 app.get("*", (req, res) => {
 	res.status(404).json({
